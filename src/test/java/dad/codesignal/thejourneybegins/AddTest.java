8412195d52ae3b5fa37f691f0ecb29a1d5ced777
IgnacioToledo21 <ignaciosurf124@gmail.com>
2024-09-12 13:03:49 +0200

    Dos Test añadidos.

--- src/test/java/dad/codesignal/thejourneybegins/AddTest.java
+++ src/test/java/dad/codesignal/thejourneybegins/AddTest.java
@@ -23,4 +23,12 @@ public class AddTest {
     public void test2() {
         assertEquals(10, add.solution(3, 7));
     }
+    @Test
+    public void test3(){
+        assertEquals(10000, add.solution(200, 9800));
+    }
+    @Test
+    public void test4(){
+        assertEquals(-50, add.solution(-48, -2));
+    }
 }
